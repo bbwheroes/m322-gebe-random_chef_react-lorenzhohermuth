7c991cc2dfda37a5c2e1f1028074187356eb4992
lorenzhohermuth <lofloho@gmail.com>
2024-01-12 10:55:29 +0100

    inital commit react vite tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-amber-300">
+      Hello world!
+    </h1>
+  )
+}
